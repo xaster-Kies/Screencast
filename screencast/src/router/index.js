d48da38acf1e3e112ef414a2ae8b1074e11b31aa
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-11 15:37:23 +0100

    r

--- screencast/src/router/index.js
+++ screencast/src/router/index.js
@@ -2,5 +2,9 @@ import Vue from "vue";
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Contact from '../views/Contact.vue'
+import VideoWatch from "../views/VideoWatch.vue";
 
-const routerHistory = createWebHistory()
+Vue.use(VueRouter);
+
+const
+
