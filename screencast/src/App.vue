73f741d92867f083e4a9ba72dfdf27bc0ea211d1
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-11 16:18:47 +0100

    g

--- screencast/src/App.vue
+++ screencast/src/App.vue
@@ -1,15 +1,6 @@
 <template>
   <div id="app">
-    <h1>Videos</h1>
-    <div class="video-container">
-      <div v-for="video in videos" :key="video.name" class="video-box">
-        <img :src="video.thumbnail" alt="img">
-        <div>
-          <h3>{{video.name}}</h3>
-          <div v-html="video.description"></div>
-        </div>
-      </div>
-    </div>
+    
   </div>
 </template>
 
---
+++ screencast/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Videos</h1>
+    <div class="video-container">
+      <div v-for="video in videos" :key="video.name" class="video-box">
+        <img :src="video.thumbnail" alt="img">
+        <div>
+          <h3>{{video.name}}</h3>
+          <div v-html="video.description"></div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
