0274821eabc06adf166fc61a1fe9bad27fdbcffa
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-11 15:52:01 +0100

    ind

--- screencast/src/router/index.js
+++ screencast/src/router/index.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import About from "../views/About.vue"
-import Contact from '../views/Contact.vue'
 import VideoWatch from "../views/VideoWatch.vue";
 
 Vue.use(VueRouter);
