3e9e6c13e49f4ed4129a120b70abce01ce35263a
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-11 15:34:20 +0100

    ind

--- screencast/src/router/index.js
+++ screencast/src/router/index.js
@@ -1,3 +1,6 @@
+import Vue from "vue";
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Contact from '../views/Contact.vue'
+
+const routerHistory = createWebHistory()
