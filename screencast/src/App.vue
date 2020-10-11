f0b85511b2e9c53b95b52dc2f4ed31303ea5a776
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-11 16:26:28 +0100

    ap

--- screencast/src/App.vue
+++ screencast/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1>hello</h1>
     <Home/>
   </div>
 </template>
--- screencast/src/views/Home.vue
+++ screencast/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="Home">
   <h1>Videos</h1>
     <div class="video-container">
       <div v-for="video in videos" :key="video.name" class="video-box">
@@ -14,10 +14,51 @@
 
 <script>
 export default {
-
+    data() {
+    return {
+      videos: [
+        {
+          name: "ES6 Arow Functions",
+          description: '<p>ES6 has some great ways to code which are easier to read and understand</p>',
+          thumbnail: 'https://i.ytimg.com/vi/h33Srr5J9nY/maxresdefault.jpg'
+        },
+        {
+          name: "Template Strings",
+          description: '<p>Template literals are string literals allowing embedded expressions. You can use multi-line strings and string interpolation features with them.</p>',
+          thumbnail: 'https://wesbos.com/static/ff07b19b6019f76c4150fe0eeec5cda5/73c85/template-strings.png'
+        },
+        {
+          name: "Promise Basics",
+          description: '<p>A promise is a special JavaScript object that links the “producing code” and the “consuming code” together. In terms of our analogy: this is the “subscription list”. The “producing code” takes whatever time it needs to produce the promised result, and the “promise” makes that result available to all of the subscribed code when it’s ready.</p>',
+           thumbnail: 'https://res.cloudinary.com/practicaldev/image/fetch/s--u31F9ZCR--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/eslcp4eursfqj1vlq8au.png'
+        },
+        {
+          name: "ES2015 Modules",
+          description: '<p>An ES6 module is a file containing JS code. There’s no special module keyword; a module mostly reads just like a script. There are two differences.</p>',
+          thumbnail: 'https://www.educative.io/api/page/5330288608542720/image/download/6288755792019456'
+        },
+      ]
+    }
 }
 </script>
 
-<style>
+<style lang='scss' scoped>
 
+
+.video-container {
+    .video-box {
+      border: 1px solid black;
+      border-radius: 10px;
+      margin: 10px;
+      padding: 10px;
+      text-align: left;
+      display: flex;
+      justify-content: flex-start;
+      img {
+        width: 200px;
+        padding: 10px;
+        
+      }
+    }
+  }
 </style>
