f4c6bbaec83250d306ffbb55f5feb73aa2a5daf5
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-11 03:28:53 +0100

    a

--- screencast/src/App.vue
+++ screencast/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
@@ -9,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
