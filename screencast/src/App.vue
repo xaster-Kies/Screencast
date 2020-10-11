974d7ee3b58d580077d46811f8083afbfdd0798b
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-11 04:23:40 +0100

    a

--- screencast/src/App.vue
+++ screencast/src/App.vue
@@ -26,8 +26,8 @@ export default {
           description: '<p>A promise is a special JavaScript object that links the “producing code” and the “consuming code” together. In terms of our analogy: this is the “subscription list”. The “producing code” takes whatever time it needs to produce the promised result, and the “promise” makes that result available to all of the subscribed code when it’s ready.</p>'
         },
         {
-          name: "Template Strings",
-          description: '<p>Template literals are string literals allowing embedded expressions. You can use multi-line strings and string interpolation features with them.</p>'
+          name: "ES2015 Modules",
+          description: '<p>An ES6 module is a file containing JS code. There’s no special module keyword; a module mostly reads just like a script. There are two differences.</p>'
         },
       ]
     }
